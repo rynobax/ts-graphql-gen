07c40a88d65644e3c096154920401832a105ff36
Ryan Baxley <ryan.baxley1@gmail.com>
2020-03-31 13:41:13 -0400

    Create failing test

--- src/generate.ts
+++ src/generate.ts
@@ -3,6 +3,5 @@ import { GraphQLSchema } from "graphql";
 import { Document } from "./types";
 
 export function generateTypes(documents: Document[], schema: GraphQLSchema) {
-  console.log(documents);
-  return "";
+  return "lol";
 }
---
+++ tests/generate.test.ts
@@ -0,0 +1,50 @@
+import { buildSchema } from "graphql";
+
+import { generateTypes } from "../src/generate";
+import { Document } from "../src/types";
+
+const schema = buildSchema(`
+schema {
+  query: Query
+}
+
+type Query {
+  me: User!
+}
+
+type User {
+  id: String!
+}
+`);
+
+const doc = (content: string): Document => ({
+  file: "somefile.ts",
+  content: content.trim(),
+});
+
+describe("generateTypes", () => {
+  test("basic", () => {
+    expect(
+      generateTypes(
+        [
+          doc(`
+    query Me {
+      me {
+        id
+      }
+    }
+    `),
+        ],
+        schema
+      )
+    ).toEqual(`
+    type MeQuery = {
+      data: {
+        me: {
+          id: String
+        }
+      }
+    }
+    `);
+  });
+});
