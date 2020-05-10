2a7a39e3fa6fb686d2c9a9bf94391c8f121d7eb4
Ryan Baxley <ryan.baxley1@gmail.com>
2020-05-10 17:05:36 -0400

    Syntax highlighting working

--- web/app.js
+++ web/app.js
@@ -1,21 +1,36 @@
 const { generateTypesString } = window.TsGraphqlGen;
-import { h, render } from "https://unpkg.com/preact?module";
-import { useState } from "https://unpkg.com/preact/hooks/dist/hooks.module.js?module";
-import htm from "https://unpkg.com/htm?module";
+import { h, render, Component } from "https://unpkg.com/preact@latest?module";
+import {
+  useRef,
+  useEffect,
+  useState,
+} from "https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module";
+import htm from "https://unpkg.com/htm@latest/dist/htm.module.js?module";
 import { demoSchema, demoDocument, demoConfig } from "./demostuff.js";
 const html = htm.bind(h);
 
+// graphql does not ship umd version
+const GQL_MODE = "text/plain";
+const TS_MODE = "text/typescript";
+
 const sections = {
-  schema: { className: "schema", title: "Schema", defaultContent: demoSchema },
+  schema: {
+    className: "schema",
+    title: "Schema",
+    defaultContent: demoSchema,
+    language: GQL_MODE,
+  },
   document: {
     className: "document",
     title: "Document",
     defaultContent: demoDocument,
+    language: GQL_MODE,
   },
   config: {
     className: "config",
     title: "Config",
     defaultContent: demoConfig,
+    language: TS_MODE,
   },
 };
 
@@ -39,35 +54,58 @@ function App() {
   return html`
     <div class="container">
       <div class="column">
-        <${Section} section=${sections.schema} language="graphql" />
-        <${Section} section=${sections.document} language="graphql" />
+        <${Section} section=${sections.schema} />
+        <${Section} section=${sections.document} />
       </div>
       <div class="column">
-        <${Section} section=${sections.config} language="typescript" />
+        <${Section} section=${sections.config} />
         <${Section}
           section=${{
             className: "output",
             title: "Output",
             defaultContent: output,
+            language: TS_MODE,
           }}
-          language="typescript"
         />
       </div>
     </div>
   `;
 }
 
-function Section({ section, language }) {
-  return html`
-    <div class="section ${section.className}">
-      <div class="header">${section.title}</div>
-      <pre class="pre">
-        <code class="language-${language}">
-          ${section.defaultContent}
-        </code>
-      </pre>
-    </div>
-  `;
+class Section extends Component {
+  constructor() {
+    super();
+    this.state = { initialized: false };
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    const { section } = this.props;
+    return html`
+      <div class="section ${section.className}">
+        <div class="header">${section.title}</div>
+        <textarea
+          ref=${(ref) => {
+            if (!ref || this.state.initialized) return;
+            this.setState({ initialized: true });
+            const codeMirror = CodeMirror.fromTextArea(ref, {
+              lineNumbers: true,
+              mode: section.language,
+              theme: "dracula",
+            });
+            setTimeout(() => {
+              codeMirror.setValue(section.defaultContent);
+              codeMirror.refresh();
+            }, 1);
+          }}
+        >
+        </textarea>
+      </div>
+    `;
+  }
 }
 
 render(html`<${App} />`, document.body);
